f4241161d97cfa6889dc5b5c7f5b91f979deb62f
ifiscoder <79508323+ifiscoder@users.noreply.github.com>
2022-07-19 23:38:33 +0530

    Update update.py

--- update.py
+++ update.py
@@ -48,8 +48,8 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     update = srun([f"git init -q \
-                     && git config --global user.email e.anastayyar@gmail.com \
-                     && git config --global user.name mltb \
+                     && git config --global user.email kashishsingh19@gnu.ac.in \
+                     && git config --global user.name ifiscoder \
                      && git add . \
                      && git commit -sm update -q \
                      && git remote add origin {UPSTREAM_REPO} \
